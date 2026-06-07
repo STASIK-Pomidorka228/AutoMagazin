99607d7b8585b343f03ec862d180911c0ba19ea6
StasikAuto <istratistas7@gmail.com>
2026-06-07 12:28:33 +0300

    cghcdhfc

---
+++ AutoMagazin/src/main/java/com/impact/AutoMagazin/AutoMagazinApplication.java
@@ -0,0 +1,13 @@
+package com.impact.AutoMagazin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMagazinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoMagazinApplication.class, args);
+	}
+
+}
---
+++ AutoMagazin/src/test/java/com/impact/AutoMagazin/AutoMagazinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impact.AutoMagazin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMagazinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
